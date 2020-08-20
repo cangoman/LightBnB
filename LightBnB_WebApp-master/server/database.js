26925006356c8159c7b9ecdbe02cf5e19264382a
Camilo Gomez <camilogomezmusic@gmail.com>
2020-08-20 15:38:19 +0000

    Updated getAllReservations functions to query the DB and return a promise

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -75,7 +75,19 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const values = [guest_id, limit];
+  return pool.query(`
+  SELECT reservations.*, properties.*, AVG(property_reviews.rating) as average_rating
+  FROM reservations 
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON property_reviews.id = reservations.property_id
+  WHERE reservations.guest_id = $1 AND end_date > now()::date
+  GROUP BY reservations.id, properties.id
+  ORDER BY start_date
+  LIMIT $2
+  `, values)
+  .then(res =>res.rows)
+  .catch(e => console.error(e.stack));
 }
 exports.getAllReservations = getAllReservations;
 
